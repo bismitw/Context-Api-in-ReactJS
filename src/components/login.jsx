40561c33cbafb8f54efe5241a659ea8471d49f71
Bismit Wagle <bismitwagle@gmail.com>
2026-01-22 14:39:30 +0530

    Basic Login Added

---
+++ src/components/login.jsx
@@ -0,0 +1,32 @@
+    import React from 'react'
+
+    function login() {
+        const[username,setUsername]=useState("");
+        const[password,setPassword]=useState("");
+
+        const handleSubmit=(e)=>{
+            //prevent page from refreshing
+        }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text" 
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username" 
+            />
+            {""}
+            <input 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Password'
+            />
+            <button
+            onClick={handleSubmit}
+            >Submit</button>
+        </div>
+    )
+    }
+
+    export default login
